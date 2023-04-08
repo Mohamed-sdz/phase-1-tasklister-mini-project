527d975c93b0f6790ce21e9145eec8e45d6661e8
saiderrahmani <didi.mousedidi@outlook.com>
2023-04-08 05:13:54 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+const name = "John"; // replace with your name
+const championNode = document.createElement("div");
+championNode.innerHTML = `${name} is the <strong>champion</strong>`;
