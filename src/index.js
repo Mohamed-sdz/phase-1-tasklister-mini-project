246ee6c91ea3a27e1ddc0c3208ce69fab6fa805f
saiderrahmani <didi.mousedidi@outlook.com>
2023-04-15 22:45:23 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const taskList = document.querySelector("#task-list");
+  const taskInput = document.querySelector("#task-input");
+  const addTaskBtn = document.querySelector("#add-task-btn");
+
+  addTaskBtn.addEventListener("click", () => {
+    const newTask = document.createElement("li");
+    newTask.innerText = taskInput.value;
+    taskList.appendChild(newTask);
+    taskInput.value = "";
+  });
+
+  taskList.addEventListener("click", (event) => {
+    if (event.target.tagName === "LI") {
+      event.target.remove();
+    }
+  });
+
+   
+     
+  
   // your code here
 });
-const name = "John"; // replace with your name
+const name = " Mohamed"; // replace with your name
 const championNode = document.createElement("div");
 championNode.innerHTML = `${name} is the <strong>champion</strong>`;
